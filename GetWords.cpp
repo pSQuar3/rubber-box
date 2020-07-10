89cb0b60fcf806f1b2df14cbae1e16af26b1c343
PratyayPande <pratyaypande@gmail.com>
2020-07-10 10:11:07 +0530

    Linear Reg.& Cf code

--- GetWords.cpp
+++ GetWords.cpp
@@ -5,13 +5,14 @@
 #include <fstream>
 #include <vector>
 using namespace std;
-int main() {
- vector<string> words;
- ifstream in("GetWords.cpp");
- string word;
- while(getline(in, word))// strings separated in the basis of new lines
- // while(in >> word) // to separate he characters of the string in space separated strnings
- words.push_back(word);
- for(int i = 0; i < words.size(); i++)
- cout << words[i] << endl;
-} ///:~ 
+int main()
+{
+    vector<string> words;
+     ifstream in("GetWords.cpp");
+     string word;
+     while(getline(in, word))// strings separated in the basis of new lines
+     // while(in >> word) // to separate he characters of the string in space separated strnings
+     words.push_back(word);
+     for(int i = 0; i < words.size(); i++)
+     cout << words[i] << endl;
+}
