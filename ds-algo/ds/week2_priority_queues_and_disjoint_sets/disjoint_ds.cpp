c08fc23ab7b6ae1fa2cb960bde47db0239875e46
PratyayPande <pratyaypande@gmail.com>
2020-08-03 13:42:36 +0530

    Updates

---
+++ ds-algo/ds/week2_priority_queues_and_disjoint_sets/disjoint_ds.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <bits/c++io.h>
+#include <algorithm>
+#include <cstdlib>
+#include <map>
+#include <string>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+struct storeSet
+{
+    vector<int> set;
+    int key;
+    storeSet()
+    {
+        key = 0;
+    }
+    void insertElements(int x)
+    {
+        if(set.empty())
+            key = x;
+        else
+            key = min(key,x);
+        set.push_back(x);
+    }
+
+};
+
+int main()
+{
+    int n,l,x,tot=0;
+    cout << "Enter number of sets: ";
+    cin >> n;
+    vector<storeSet> sets;
+    for(int i=0;i<n;i++)
+    {
+        storeSet ob;
+        cout << "Enter length of first set: ";
+        cin >> l;
+        tot += l;
+        for(int k=0;k<l;k++)
+        {
+            cin >> x;
+            ob.insertElements(x);
+        }
+        sets.push_back(ob);
+    }
+    return 0;
+}
+
+
