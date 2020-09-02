6b31172b4e06d8e71ae0b692b79a6b988d46fac7
pSQuar3 <pratyaypande@gmail.com>
2020-09-02 14:47:31 +0530

    Maze solution

--- ds-algo/graphs/week1_graph_decomposition1/1_finding_exit_from_maze/secReach.cpp
+++ ds-algo/graphs/week1_graph_decomposition1/1_finding_exit_from_maze/secReach.cpp
@@ -7,25 +7,57 @@ using std::ios;
 using std::unordered_map;
 using std::vector;
 using std::pair;
-using std::make_pair;
-using std::hash;
-struct hash_pair
+using std::endl;
+int n,m,x,y,c=1;
+unordered_map<int, vector<int>> g;
+unordered_map<int, bool> rch;
+void check(int current)
 {
-    template <class T1, class T2>
-    size_t operator()(const pair<T1, T2>& p) const
+    rch[current] = true;
+    cout << current << " ";
+    for(auto i=g[current].begin();i!=g[current].end();i++)
     {
-        auto hash1 = hash<T1>{}(p.first);
-        auto hash2 = hash<T2>{}(p.second);
-        return hash1 ^ hash2;
+        if(rch[*i] == false && *i != x)
+            return check(*i);
     }
-};
-//currently mapped s: pair<any dt,any dt>->bool with custom hash function as hash_pair
-//change data types of pair and mapped value accordingly
-unordered_map<pair<int,int>,bool,hash_pair> mp;
-
+}
+bool checkGr(int current)
+{
+    //cout << "node = " << current<< endl;
+    rch[current] = true;
+    //cout << current << " ";
+    bool b = false;
+    if(current == y)
+        b = true;
+    for(auto i=g[current].begin();!b && i!=g[current].end();i++)
+    {
+        if(rch[*i] == false)
+            b = b + checkGr(*i);
+    }
+    return b;
+}
 void solve()
 {
-    
+    std::cin >> n >> m;
+    for(int i=1;i<=n;i++)
+    {
+        g[i] = {};
+        rch[i] = false;
+    }
+    for(int i=0;i<m;i++)
+    {
+        cin >> x >> y;
+        g[x].push_back(y);
+        g[y].push_back(x);
+    }
+    cin >> x >> y;
+    //cout << "direct connections to the node to be searched:" << endl;
+    //for(auto i=g[x].begin();i!=g[x].end();i++)
+    //    cout << *i << endl;
+    //cout << "displayed accessible elements of graph from x:" << endl;
+    //check(x);
+    //cout << "\nis accessible?" << endl;
+    cout << checkGr(x); 
 }
 int main()
 {
