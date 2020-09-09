ec0e71203ed1981e124030528703fe239e1a0d4b
pSQuar3 <pratyaypande@gmail.com>
2020-09-09 08:01:55 +0530

    Changes and updates + pruning some files

--- ds-algo/graphs/week3_paths_in_graphs1/1_flight_segments/bfs.cpp
+++ ds-algo/graphs/week3_paths_in_graphs1/1_flight_segments/bfs.cpp
@@ -1,27 +1,52 @@
-#include <iostream>
-#include <vector>
-#include <queue>
-
+#include <bits/stdc++.h>
 using std::vector;
 using std::queue;
-
-int distance(vector<vector<int> > &adj, int s, int t) {
-  //write your code here
-  return -1;
+using std::cin;
+using std::cout;
+using std::endl;
+int n,m;
+int bipartite(vector<vector<int> > &adj)
+{
+    //write your code here
+    int x,y,u;
+    cin >> x >> y;
+    int dist[n];
+    for(int i=0;i<n;i++)
+        dist[i] = -1;
+    dist[x-1] = 0;
+    queue<int> qu;
+    qu.push(x-1);
+    bool b = false;
+    while(!qu.empty())
+    {
+        u = qu.front();
+        qu.pop();
+        //cout << "discovered edge = " << u << endl;
+        for(auto r = adj[u].begin();!b && r!=adj[u].end();r++)
+        {
+            if(dist[*r] == -1)
+            {
+                qu.push(*r);
+                dist[*r] = dist[u]+1;
+            }
+            if(*r == y-1)
+                b = true;
+        }
+    }
+    return dist[y-1];
 }
 
-int main() {
-  int n, m;
-  std::cin >> n >> m;
-  vector<vector<int> > adj(n, vector<int>());
-  for (int i = 0; i < m; i++) {
-    int x, y;
-    std::cin >> x >> y;
-    adj[x - 1].push_back(y - 1);
-    adj[y - 1].push_back(x - 1);
-  }
-  int s, t;
-  std::cin >> s >> t;
-  s--, t--;
-  std::cout << distance(adj, s, t);
+int main()
+{
+    std::cin >> n >> m;
+    vector<vector<int> > adj(n, vector<int>());
+    for (int i = 0; i < m; i++)
+    {
+        int x, y;
+        std::cin >> x >> y;
+        adj[x - 1].push_back(y - 1);
+        adj[y - 1].push_back(x - 1);
+    }
+    std::cout << bipartite(adj);
+    return 0;
 }
--- ds-algo/graphs/week3_paths_in_graphs1/2_bipartite/bipartite.cpp
+++ ds-algo/graphs/week3_paths_in_graphs1/2_bipartite/bipartite.cpp
@@ -1,24 +1,66 @@
-#include <iostream>
-#include <vector>
-#include <queue>
-
+#include <bits/stdc++.h>
 using std::vector;
 using std::queue;
+using std::cin;
+using std::cout;
+using std::endl;
+int n,m;
 
-int bipartite(vector<vector<int> > &adj) {
-  //write your code here
-  return -1;
+void bipartite(vector<vector<int> > &adj)
+{
+    //write your code here
+    int x,y,u;
+    cin >> x >> y;
+    vector<int> dist,prev;
+    for(int i=0;i<n;i++)
+    {
+        dist[i] = -1;
+        prev[i] = -1;
+    }
+    dist[x-1] = 0;
+    queue<int> qu;
+    qu.push(x-1);
+    bool b = false;
+    while(!qu.empty())
+    {
+        u = qu.front();
+        qu.pop();
+        //cout << "discovered edge = " << u << endl;
+        for(auto r = adj[u].begin();!b && r!=adj[u].end();r++)
+        {
+            if(dist[*r] == -1)
+            {
+                qu.push(*r);
+                dist[*r] = dist[u]+1;
+                prev[*r] = u;
+            }
+            if(*r == y-1)
+                b = true;
+        }
+    }
 }
-
-int main() {
-  int n, m;
-  std::cin >> n >> m;
-  vector<vector<int> > adj(n, vector<int>());
-  for (int i = 0; i < m; i++) {
-    int x, y;
-    std::cin >> x >> y;
-    adj[x - 1].push_back(y - 1);
-    adj[y - 1].push_back(x - 1);
-  }
-  std::cout << bipartite(adj);
+vector<int> Reconstruct(int S, int u, vector<int> &prev)
+{
+    vector<int>  res;
+    while(u != S)
+    {
+        res.push_back(u);
+        u = prev[u];
+    }
+    return res;
+}
+int main()
+{
+    std::cin >> n >> m;
+    vector<vector<int> > adj(n, vector<int>());
+    for (int i = 0; i < m; i++)
+    {
+        int x, y;
+        std::cin >> x >> y;
+        adj[x - 1].push_back(y - 1);
+        adj[y - 1].push_back(x - 1);
+    }
+    bipartite(adj);
+    vector<int>
+    return 0;
 }
