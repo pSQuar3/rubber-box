2eb2610fafdc4006775342ab468c3d3dbb6454c5
pSQuar3 <pratyaypande@gmail.com>
2020-09-21 23:58:54 +0530

    Bellman Ford attempt Need to re-attempt algorithm

--- ds-algo/graphs/week4_paths_in_graphs2/2_detecting_anomalies/negative_cycle.cpp
+++ ds-algo/graphs/week4_paths_in_graphs2/2_detecting_anomalies/negative_cycle.cpp
@@ -1,18 +1,43 @@
-#include <iostream>
-#include <vector>
-
+#include <bits/stdc++.h>
 using std::vector;
+int n,m;
 
-int negative_cycle(vector<vector<int> > &adj, vector<vector<int> > &cost) {
+int negative_cycle(vector<vector<int>> &adj, vector<vector<int>> &cost)
+{
     //write your code here
+    int dist[n];
+    int x;
+    std::cin >> x;
+    for(int i=0;i<n;i++)
+    {
+        dist[i] = INT_MAX;
+    }
+    dist[0] = 0;
+    /*for(int e=0;e<n;e++)
+    {*/
+    for(int v = 0;v < n-1;v++)
+        for(int i=0;i<n;i++)
+        {
+            for(int j = 0;j < (int)adj[i].size();i++)
+            {
+                //  i -> value of the node
+                std::cout << "i = " << i << " node = " << adj[i][j] << std::endl;
+                if(dist[adj[i][j]] > dist[i] + cost[i][j])
+                {
+                    dist[adj[i][j]] = dist[i] + cost[i][j];
+                }
+            }
+        }
+    //}
+    std::cout << "distance = " << dist[x] << std::endl;
     return 0;
 }
 
-int main() {
-    int n, m;
+int main()
+{
     std::cin >> n >> m;
-    vector<vector<int> > adj(n, vector<int>());
-    vector<vector<int> > cost(n, vector<int>());
+    vector<vector<int>> adj(n, vector<int>());
+    vector<vector<int>> cost(n, vector<int>());
     for (int i = 0; i < m; i++)
     {
         int x, y, w;
