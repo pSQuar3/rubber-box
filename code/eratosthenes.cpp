e001eca7faf3afd7b0e64d9637d4ecc52b55700e
pSQuar3 <pratyaypande@gmail.com>
2020-11-20 08:03:46 +0530

    Sieve of Eratosthenes

--- code/eratosthenes.cpp
+++ code/eratosthenes.cpp
@@ -3,34 +3,8 @@
 #define lli long long int
 #define ull unsigned long long int
 
-using std::cin;
-using std::cout;
-using std::cerr;
-using std::endl;
-using std::floor;
-using std::ceil;
-using std::vector;
-using std::unordered_map;
-using std::map;
-using std::stack;
-using std::queue;
-using std::max;
-using std::min;
-using std::deque;
-using std::priority_queue;
-using std::bitset;
-using std::set;
-using std::sort;
-using std::hash;
-using std::pair;
-using std::make_pair;
-using std::string;
-using std::swap;
-const int MX = 100000;
+using namespace std;
 int n;
-//int D[MX+1][MX+1];
-//int D[MX+1];
-//int a[MX];
 vector<int> buildSieve()
 {
     vector<int> sieve;
@@ -55,7 +29,7 @@ inline void solve()
 }
 
 int main()
-{//-
+{
     std::ios_base::sync_with_stdio(0);
     cin.tie(0);
     int t = 1;
