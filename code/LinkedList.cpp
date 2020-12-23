a33fad1f8827be7ab19e99ec4accad986ba8d984
pSQuar3 <pratyaypande@gmail.com>
2020-12-23 20:54:52 +0530

    Second rough commit

--- code/LinkedList.cpp
+++ code/LinkedList.cpp
@@ -1,51 +1,96 @@
-#include <iostream>
-#include <new>
-
+#include <bits/stdc++.h>
+#define lli long long int
+#define ull unsigned long long
 using namespace std;
+template<typename A,typename B>
+inline A fast_power(A x,B y){if(y<=1)return x;A k=fast_power(x,y/2);k=k*k;if(y%2==1)k=k*x;return k;}
+template<typename A>
+inline int gcd(A a,A b){A t;while((a!=0)&&(b!=0)){t=a%b;a=b;b=t;}return a+b;}
+template<typename A,typename B>
+inline lli ncr(A x,B r){lli top=1,bot=1;while(r>=1){top*=(x-r+1);bot*=r;r--;} return top/bot;}
+template<typename A>
+inline pair<A,int> kadane(A x[], int n) {
+    A max_so_far = -9999999999999, max_ending_here = 0; int h = 0;
+    for(int i=0;i<n;i++) { max_ending_here += x[i];
+        if(max_so_far < max_ending_here) { max_so_far = max_ending_here; h = i; }
+        if(max_ending_here < 0) max_ending_here = 0;}
+    return make_pair(max_so_far,h);
+}
+struct custom_hash
+{
+    template <class T1, class T2>
+    size_t operator()(const pair<T1, T2>& p) const
+    { auto hash1 = hash<T1>{}(p.first); auto hash2 = hash<T2>{}(p.second); return hash1 ^ hash2; }
+    template <typename T1>
+    size_t operator()(const T1& p) const
+    { auto hash1 = hash<T1>{}(p); return hash1;
+        /* auto hash2 = hash<T1>{}(p.first); return hash1^hash2; */ }
+};
 struct Node
 {
-    int data;
+    int val;
     Node *next;
-};
-Node *HEAD = NULL;
-void insert(int n)
-{
-    Node *k = (Node*)malloc(sizeof(Node));
-    k->data = n;
-    k->next = HEAD;
-    HEAD = k;
-}
-void display()
-{
-    for(Node *j = HEAD; j != NULL;j = j->next)
-        cout << j->data << " ";
-    cout << endl;
-}
-void UltaFy()
-{
-    //reversing a linked list
-    Node *x = NULL,*k=NULL;
-    cout << "reversed" << endl;
-    Node *i = HEAD;
-    while(i != NULL)
+    Node(int v)
     {
-        cout << "data = " << i->data << "\n";
-        k = i->next;
-        i->next = x;
-        x = i;
-        i = k;
+        val = v;
+        next = NULL;
     }
-    HEAD = x;
+};
+void delNode(Node *nd)
+{
+    while(nd->next != NULL)
+    {
+        nd->val = nd->next->val;
+        nd = nd->next;
+    }
+    nd->next = NULL;
+}
+void disp(Node *head)
+{
+    while(head->next != NULL)
+    {
+        cout << head->val << " ";
+        head = head->next;
+    }
+}
+void solve()
+{
+    int n;
+    cin >> n;
+    Node *head;
+    Node *nd;
+    bool h = true;
+    Node *a;
+    while(n--)
+    {
+        int x;
+        cin >> x;
+        if(h)
+        {
+            head = new Node(x);
+            nd = head;
+        }
+        else
+        {
+            a = new Node(x);
+            nd->next = a;
+            nd = a;
+        }
+    }
+    cin >> n;
+    nd = head;
+    while(n--)
+        nd = nd->next;
+    delNode(nd);
+    disp(head);
 }
 int main()
 {
-    int n;
-    cout << "Enter limit: ";
-    cin >> n;
-    for(int i=0;i<n;i++)
-        insert(i+1);
-    display();
-    UltaFy();
-    display();
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int t = 1;
+    //cin >> t;
+    while(t--)
+        solve();
     return 0;
 }
