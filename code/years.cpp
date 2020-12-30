8fd6770c316a75f0085dc079dc8988b004a4cc62
pSQuar3 <pratyaypande@gmail.com>
2020-12-30 15:42:22 +0530

    Year Question

--- code/years.cpp
+++ code/years.cpp
@@ -3,29 +3,7 @@
 #define lli long long int
 #define ull unsigned long long int
 
-using std::cin;
-using std::cout;
-using std::cerr;
-using std::endl;
-using std::floor;
-using std::ceil;
-using std::vector;
-using std::unordered_map;
-using std::map;
-using std::stack;
-using std::queue;
-using std::max;
-using std::min;
-using std::deque;
-using std::priority_queue;
-using std::bitset;
-using std::set;
-using std::sort;
-using std::hash;
-using std::pair;
-using std::make_pair;
-using std::string;
-using std::swap;
+using namespace std;
 const int MX = 100000;
 int n;
 //int D[MX+1][MX+1];
@@ -35,6 +13,7 @@ int n;
 inline void solve()
 {
     cin >> n;
+    cout << n << "\n";
 }
 
 int main()
