166e66283fd5c9c7bf9f7c36f7eaf830ad0d802f
pSQuar3 <pratyaypande@gmail.com>
2021-02-17 08:42:38 +0530

    Trimmed some files

---
+++ code/odd_div.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+#define lli long long int
+#define ull unsigned long long
+using namespace std;
+const int INF = 10000;
+const int MX = 100000;
+const double MOD = log(2);
+template<typename A,typename B>
+lli fpow(A x,B y,long M)
+{
+    if(y == 1) return x%M;
+    if(y == 0) return 1;
+    A k = fpow(x, y/2, M);
+    if(y%2 == 0)
+        return (k * k) % M;
+    return (((k * k) % M) * x) % M;
+}
+template<typename A>
+inline int gcd(A a,A b){A t;while((a!=0)&&(b!=0)){t=a%b;a=b;b=t;}return a+b;}
+template<typename A,typename B>
+inline long modular_inverse(A n,B M) { return fpow<A,B>(n,M-2,(long)M); }
+template<typename A,typename B>
+inline lli ncr(A x,B r,long M) {
+    if(r == 0) return 1; lli v = ncr(x-1,r-1,M);
+    v = (x*v)%M; v = (x*modular_inverse<A,B>(r,M))%M; return v;
+}
+struct custom_hash
+{
+    template <class T1, class T2>
+    size_t operator()(const pair<T1, T2>& p) const
+    {
+        auto hash1=hash<T1>{}(p.first);auto hash2=hash<T2>{}(p.second);
+        return hash1^hash2;
+    }
+};
+int mxkq=0;
+inline void buildSieve(int xw,int sieve[]) 
+{
+    for(int i=mxkq;i<=max(xw,mxkq);i++)
+        sieve[i] = 0;
+    for(int i = max(2,mxkq); i <= xw;i++)
+    {
+        if(sieve[i])
+            continue;
+        for(int j = 2*i;j <= xw;j += i)
+            sieve[j] = i;
+    }
+    mxkq = max(mxkq,xw);
+}
+auto cmp = [](pair<int,int> left, pair<int,int> right)
+{
+    return(left.second < right.second);
+};
+bool checkLog(lli k)
+{
+    double j = ((double)log(k))/MOD;
+    if(j - floor(j) == 0)
+        return true;
+    return false;
+}
+int main()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0); int t = 1;
+    cin >> t;
+    while(t--)
+    {
+        lli n;
+        cin >> n;
+        if(n&(n-1))
+            cout << "YES\n";
+        else
+            cout << "NO\n";
+    }
+    return 0;
+}
--- ds-algo/strings/week1/pattern.sh
+++
@@ -1,4 +0,0 @@
-g++ -std=c++17 -Wshadow -Wall randomLineGenerator.cpp -O2
-time ./a.out
-g++ -std=c++17 -Wshadow -Wall trie_matching.cpp -O2
-time ./a.out
